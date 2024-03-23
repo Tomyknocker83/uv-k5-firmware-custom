71e550012fd9e9f578e8f9aaa6c880d99dd12b4d
Tomyknocker83 <96396518+Tomyknocker83@users.noreply.github.com>
2024-03-23 18:17:01 +0100

    Update frequencies.c

--- frequencies.c
+++ frequencies.c
@@ -223,6 +223,9 @@ int32_t TX_freq_check(const uint32_t Frequency)
 			break;
 
 		case F_LOCK_ALL:
+			for (uint32_t i = 0; i < ARRAY_SIZE(frequencyBandTable); i++)
+				if (Frequency >= frequencyBandTable[i].lower && Frequency < frequencyBandTable[i].upper)
+					return 0;
 			break;
 
 		case F_LOCK_NONE:
